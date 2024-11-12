419250c60583c315c25a4f000c7e6f099c0822aa
fran <fran22345@gmail.com>
2024-11-12 10:28:14 -0300

    createservice

--- app/views/createService.js
+++ app/views/createService.js
@@ -1,18 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import { View, StyleSheet, Button, Alert } from "react-native";
 import { Stack } from "expo-router";
 import { TextInput } from "react-native-paper";
 import axios from "axios";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
 
 const CreateService = () => {
-  const [serviceName, setServiceName] = useState("");
+  const [serviceProfession, setServiceProfession] = useState("");
   const [serviceDescription, setServiceDescription] = useState("");
   const [servicePrice, setServicePrice] = useState("");
+  const [state, setState] = useState({ currentUser: null });
+
+  const getCurrentUser = async () => {
+    try {
+      await GoogleSignin.configure(); 
+      const currentUser = await GoogleSignin.getCurrentUser(); 
+      
+      setState({ currentUser });
+    } catch (error) {
+      console.error("Error getting current user", error);
+    }
+  };
+
+  useEffect(() => {
+    getCurrentUser();    
+  }, []);
 
   const handleSubmit = async () => {
     try {
-      const response = await axios.post("http://10.0.2.2:3000/services", {
-        name: serviceName,
+      const response = await axios.post("http://10.0.2.2:3000/users", {
+        nombre: state.currentUser.user.givenName,
+        apellido: state.currentUser.user.familyName,
+        profesion: serviceProfession,
+        linkFoto: state.currentUser.user.photo,
         description: serviceDescription,
         price: parseFloat(servicePrice),
       });
@@ -27,9 +47,9 @@ const CreateService = () => {
     <View style={styles.container}>
       <Stack.Screen options={{ title: "Crear Servicio" }} />
       <TextInput
-        label="Nombre del Servicio"
-        value={serviceName}
-        onChangeText={(text) => setServiceName(text)}
+        label="Profesión"
+        value={serviceProfession}
+        onChangeText={(text) => setServiceProfession(text)}
         style={styles.input}
       />
       <TextInput
