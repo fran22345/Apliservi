3ed65d13649b3d8a5354568e59ca73ff9c85ab6d
fran <fran22345@gmail.com>
2024-11-18 18:05:45 -0300

    usepush

--- app/index.js
+++ app/index.js
@@ -1,13 +1,23 @@
 import React, { useEffect } from "react";
 import { useNavigation } from "expo-router";
 import HomeScreen from "./views/homeScreen";
+import { View, Text } from "react-native";
+import usePushNotification from "../usePushNotification";
 
 export default function App() {
-  
+  const { expoPushToken, notification } = usePushNotification();
+  const data = JSON.stringify({ notification });
+
   const navigation = useNavigation();
   useEffect(() => {
     navigation.setOptions({ headerShown: false });
   }, [navigation]);
 
-  return <HomeScreen />;
+  return (
+    <View>
+      <Text>Token: {expoPushToken?.data ?? ""}</Text>;
+      <Text>{data}</Text>;
+      <HomeScreen />;
+    </View>
+  );
 }
